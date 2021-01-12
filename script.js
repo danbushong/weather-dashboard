0a561caca09450275c9e3ecd2ce00ada6950baff
Dan Bushong <bushongdan@gmail.com>
2021-01-12 15:39:28 -0600

    working on getting submit to stay on page

--- script.js
+++ script.js
@@ -1,15 +1,54 @@
+
+
+
+//what will happen when the page loads
 $(document).ready(function () {
 
+    function show(data) {
+
+
+
+
+    }
+    function showUV(data) {
+
+
+
+
+    }
+    function displayCities(data) {
+
+
+
+
+    }
+    function showForecast(data) {
 
 
 
 
 
 
+    }
 
-    
+
+    //stored list on the side
+    var localStored = localStorage.getItem("cityList");
+    //if something is stored display it
+    if (stored) {
+        cityList = JSON.parse(stored)
+    } else {
+        cityList = []
+    }
+
+
+
+
+
+
 
 
 
 });
+
 
