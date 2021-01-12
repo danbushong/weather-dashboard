55e3f737771265aec3aa928078c1e70bb83fa266
Dan Bushong <bushongdan@gmail.com>
2021-01-12 17:48:46 -0600

    switched to the ajax functions

--- script.js
+++ script.js
@@ -41,6 +41,53 @@ $(document).ready(function () {
         cityList = []
     }
 
+    //what happens when submit/search gets clicked
+    $('#submitCity').click(function (event) {
+        //stop it from submitting the page and reloading
+        event.preventDefault();
+
+        var city = $('#city').val();
+
+        cityList.push(city);
+
+        localStorage.setItem("cityList", JSON.stringify(cityList));
+
+        displayCities(cityList);
+    });
+    
+    if(city != ""){
+        $.ajax({
+            url: "api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial"+"&appid=fd591006a7dae5562993f7f902a78b07",
+            type: "GET",
+            success: function (data){
+                var display = showForecast(data);
+                
+
+
+            }
+
+        })
+        $.ajax({
+            url: "api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial"+"&appid=fd591006a7dae5562993f7f902a78b07",
+            type: "GET",
+            success: function (data){
+                var forecastDisplay = showForcast(data)
+
+            }
+            
+        })
+        $.ajax({
+            url: "api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial"+"&appid=fd591006a7dae5562993f7f902a78b07",
+            type: "GET",
+            success: function (data){
+                var forecastDisplay = showForcast(data)
+
+
+            }
+            
+        })
+    }
+
 
 
 
