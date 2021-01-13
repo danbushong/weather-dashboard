f66b5fbce15e653c5e04c3239ddc554e693d130f
Dan Bushong <bushongdan@gmail.com>
2021-01-13 16:53:15 -0600

    pretty much functional, past searches are now clickable

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    var operatorClicked = false;
+
     // FUNCTIONS
     function show(data) {
         return "<h2>" + data.name + moment().format(' (MM/DD/YYYY)') + "</h2>" +
@@ -9,40 +11,91 @@ $(document).ready(function () {
         <p><strong>Wind Speed</strong>: ${data.wind.speed} MPH</p>
         `
     }
-    
+
 
     function displayCities(cityList) {
         $('.city-list').empty();
         var list = localStorage.getItem("cityList");
         cityList = (JSON.parse(list));
+
         // add a new div to fit how many things are in local storage
         if (list) {
             for (var i = 0; i < cityList.length; i++) {
-                var container = $("<button>test</button>").click(function(){
+                var container = $("<button class=historyButton name=" + cityList[i] + ">" + cityList[i] + "</button>").click(function (event) {
+
+
+
 
                 })
                 //add to the top of list
                 $('.city-list').prepend(container);
             }
+            //this will make it so if a past location is clicked it will load the info for it using the text of the button
+            $(".historyButton").on("click", function (event) {
+                event.preventDefault();
+
+                //changes every button to say the text of the button clicked
+                var city = $(this).text();
+ 
+
+                if (city != '') {
+                    //display current city information
+                    $.ajax({
+                        url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&appid=fd591006a7dae5562993f7f902a78b07",
+                        type: "GET",
+                        success: function (data) {
+                            var display = show(data);
+                            $("#show").html(display);
+                        }
+                    });
+                    //display 5 day forcast information
+                    $.ajax({
+                        url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial" + "&appid=fd591006a7dae5562993f7f902a78b07",
+                        type: "GET",
+                        success: function (data) {
+                            showForecast(data)
+
+                        }
+                    });
+
+
+                } else {
+                    $('#error').html('Please insert a city name:');
+                }
+
+
+            })
+
+
         }
     }
 
+
+
+    //brings in all the data about austin
     function showForecast(data) {
-        var forecast = data.list; 
+
+
+        var forecast = data.list;
+
+
+
         var currentForecast = [];
         for (var i = 0; i < forecast.length; i++) {
 
             var currentObject = forecast[i];
-            
 
-            var dt_time = currentObject.dt_txt.split(' ')[1] 
+
+
+            var dt_time = currentObject.dt_txt.split(' ')[1]
             if (dt_time === "12:00:00") {
-                
+
                 var main = currentObject.main;
-                
-                var temp = main.temp; 
+
+
+                var temp = main.temp;
                 var humidity = main.humidity;
-                var date = moment(currentObject.dt_txt).format('l'); 
+                var date = moment(currentObject.dt_txt).format('l');
                 var icon = currentObject.weather[0].icon;
                 var iconurl = "https://openweathermap.org/img/w/" + icon + ".png";
 
@@ -65,7 +118,7 @@ $(document).ready(function () {
 
     }
 
-    
+
     //add stored to the page 
     var stored = localStorage.getItem("cityList")
 
@@ -73,23 +126,26 @@ $(document).ready(function () {
     if (stored) {
         //load past searched cities
         cityList = JSON.parse(stored)
-        
-    } 
+
+    }
     else {
         cityList = []
     }
 
 
-    
+
+
+
     //puts it into the city list and then display current city and 5 day forcast
     $('#submitCity').click(function (event) {
         event.preventDefault();
+        //this is where the value of city gets stored in order to search the data
         var city = $('#city').val();
-       
+
         cityList.push(city);
-        
+
         localStorage.setItem("cityList", JSON.stringify(cityList));
-        
+
         displayCities(cityList);
 
         if (city != '') {
@@ -108,10 +164,10 @@ $(document).ready(function () {
                 type: "GET",
                 success: function (data) {
                     var forecastDisplay = showForecast(data)
-                    
+
                 }
             });
-           
+
 
         } else {
             $('#error').html('Please insert a city name:');
